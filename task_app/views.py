e41f7ad0f9c5fc65f6170e13d64c0ef248814a17
ShutenkoStas <shotenko494@gmail.com>
2025-12-23 01:36:03 +0200

    Add urls, views and templates

--- task_app/views.py
+++ task_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from task_app import models
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+class TaskListView(ListView):
+    model = models.Task
+    context_object_name = "tasks"
+    template_name = "tasks/task_list.html"
+
+class TaskDetailView(DetailView):
+    model = models.Task
+    context_object_name = "tasks"
+    template_name = "tasks/task_detail.html"
+    
+
+
