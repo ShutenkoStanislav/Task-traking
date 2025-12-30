a1064f4168b625050ce2fa3346c048bd135d3d48
ShutenkoStas <shotenko494@gmail.com>
2025-12-30 20:05:52 +0000

    Add better desing for task form, connect new version of bootstrap

--- task_app/forms.py
+++ task_app/forms.py
@@ -4,21 +4,29 @@ from task_app.models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ["title", "description", "status", "priority", "due_date"]  
+        fields = ["title", "description", "status", "priority", "due_date"] 
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": "form-control"})
+
+        self.fields['due_date'].widget.attrs["class"] += " my-custom=datepicker"
+            
+                 
 
 
 class TaskFilterForm(forms.Form):
-        STATUS_CHOICES = [
-        ("", "All"),
-        ("todo", "To Do"),
-        ("in_progress", "In progress"),
-        ("done", "Done"),
-        ]
+    STATUS_CHOICES = [
+    ("", "All"),
+    ("todo", "To Do"),
+    ("in_progress", "In progress"),
+    ("done", "Done"),
+    ]
         
-        status = forms.ChoiceField(choices=STATUS_CHOICES, required=False, label="Status")
+    status = forms.ChoiceField(choices=STATUS_CHOICES, required=False, label="Status")
 
-        def __init__(self, *args, **kwargs):
-            super(TaskFilterForm, self).__init__(*args, **kwargs)
-            self.fields['status'].widget.attrs.update({"class": "form-control"})
+    def __init__(self, *args, **kwargs):
+        super(TaskFilterForm, self).__init__(*args, **kwargs)
+        self.fields['status'].widget.attrs.update({"class": "form-control"})
 
     
